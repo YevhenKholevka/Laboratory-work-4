59a948eeb9ff568525f6a041b8a694efe520a886
YevhenKholevka <127884132+YevhenKholevka@users.noreply.github.com>
2024-11-14 20:31:17 +0200

    Update RelationDB_DetailedDataSchemaModified.sql

--- RelationDB_DetailedDataSchemaModified.sql
+++ RelationDB_DetailedDataSchemaModified.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Employee (
+CREATE TABLE Employeу (
     Id INT PRIMARY KEY,
     Surname VARCHAR(50) NOT NULL CHECK (Surname ~ '^[A-Z][a-z]{1,49}$'),
     Position VARCHAR(50) NOT NULL CHECK (Position ~ '^[A-Za-z ]{2,50}$')
@@ -19,15 +19,15 @@ CREATE TABLE System (
 CREATE TABLE Recommendation (
     Id INT PRIMARY KEY,
     Text TEXT NOT NULL CHECK (Text ~ '^[A-Za-z0-9,. ]{5,255}$'),
-    System_id INT,
+    Systemid INT,
     FOREIGN KEY (System_id) REFERENCES System (Id)
 );
 
 CREATE TABLE Notification (
     Id INT PRIMARY KEY,
     Type VARCHAR(50) NOT NULL CHECK (Type IN ('safety', 'risk')),
-    Send_date DATETIME NOT NULL,
-    System_id INT,
+    Senddate DATETIME NOT NULL,
+    Systemid INT,
     FOREIGN KEY (System_id) REFERENCES System (Id)
 );
 
@@ -45,7 +45,7 @@ CREATE TABLE Riskassessment (
     Id INT PRIMARY KEY,
     Result VARCHAR(255) NOT NULL CHECK (Result ~ '^[A-Za-z0-9,. ]{10,255}$'),
     Date DATE NOT NULL,
-    Manager_id INT,
+    Managerid INT,
     FOREIGN KEY (Manager_id) REFERENCES Manager (Id)
 );
 
@@ -53,6 +53,6 @@ CREATE TABLE Risk (
     Id INT PRIMARY KEY,
     Level VARCHAR(50) CHECK (Level IN ('low', 'medium', 'high')),
     Type VARCHAR(50) NOT NULL,
-    Risk_assessment_id INT,
+    Riskassessmentid INT,
     FOREIGN KEY (Risk_assessment_id) REFERENCES Riskassessment (Id)
 );
